2e9d82318b165525af357b3017ddf0e4249ce7e6
drewminns <drew.minns@me.com>
2018-04-15 09:22:27 -0400

    added tests for Checkbox and Count

---
+++ client/scripts/__tests__/Checkbox.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Checkbox from '../Components/Checkbox';
+
+const LABEL = 'LABEL TEXT';
+const mockCallback = jest.fn();
+
+describe('<Checkbox />', () => {
+  it('Renders Correctly', () => {
+    const checkbox = renderer.create(<Checkbox />).toJSON();
+    expect(checkbox).toMatchSnapshot();
+  });
+
+  it('Renders a label', () => {
+    const checkbox = shallow(<Checkbox label={LABEL} />);
+    expect(checkbox.text()).toBe(LABEL);
+  });
+
+  it('Calls a function when clicked', () => {
+    const checkbox = shallow(
+      <Checkbox label={LABEL} onChange={mockCallback} />
+    );
+    checkbox.find('input.Checkbox__Input').simulate('change');
+    expect(mockCallback).toBeCalled();
+  });
+});
