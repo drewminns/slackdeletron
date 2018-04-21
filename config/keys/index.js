08fc882d5ac07d27142b654f76b6336944d16f5e
drewminns <drew.minns@me.com>
2018-04-21 15:52:30 -0400

    updated tests to use fake keys

--- config/keys/index.js
+++ config/keys/index.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
+} else if (process.env.NODE_ENV === 'test') {
+  module.exports = require('./testKeys');
 } else {
   module.exports = require('./dev');
 }
---
+++ config/keys/testKeys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  slackClientID: '123456789.987654321',
+  slackClientSecret: '1a2s3d4f5g6h7j8k9l',
+  cookieKey: '9l9k8j7h6g5f4d3s2a1a',
+};
