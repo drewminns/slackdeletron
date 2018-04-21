e55d08bc0df2c3a854a555f305027e16be6ae485
drewminns <drew.minns@me.com>
2018-04-21 14:21:23 -0400

    Using Slack Client ID for Add To Slack

--- client/scripts/Containers/SignIn.js
+++ client/scripts/Containers/SignIn.js
@@ -51,7 +51,7 @@ export default class SignIn extends Component {
         </p>
         <a
           href={`https://slack.com/oauth/authorize?client_id=${
-            keys.distributionToken
+            keys.slackClientID
           }&scope=users:read,files:read,files:write:user,channels:read`}
         >
           <img
@@ -59,7 +59,7 @@ export default class SignIn extends Component {
             height="40"
             width="139"
             src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
           />
         </a>
         <p>
@@ -69,7 +69,6 @@ export default class SignIn extends Component {
         </p>
       </Fragment>
     );
-
     const backFace = (
       <Fragment>
         <button onClick={this.toggleFAQ} className="SignIn__button">
