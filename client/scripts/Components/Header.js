3aeb4ce36d3d454b79b04b5c70a85100f2796881
drewminns <drew.minns@me.com>
2018-04-21 16:17:06 -0400

    Changed to sign in buttons

--- client/scripts/Components/Header.js
+++ client/scripts/Components/Header.js
@@ -21,15 +21,14 @@ const Header = ({ isLoggedIn = false, name = '', avatar = '' }) => {
         <a
           className="LoginButton"
           href={`https://slack.com/oauth/authorize?client_id=${process.env
-            .SLACK_CLIENT_ID ||
-            keys.slackClientID}&scope=users:read,files:read,files:write:user,channels:read`}
+            .SLACK_CLIENT_ID || keys.slackClientID}&scope=identity.basic`}
         >
           <img
-            alt="Add to Slack"
+            alt="Sign in with Slack"
             height="40"
-            width="139"
-            src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            width="172"
+            src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+            srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
           />
         </a>
       </Fragment>
